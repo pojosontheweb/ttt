c05cd52936cb388d5a54a27549ec315311c9b5bd
Remi Vankeisbelck <remi@rvkb.com>
2015-07-21 02:12:08 +0200

    antlr attempt TBC

--- core/src/main/antlr4/com/pojosontheweb/ttt/Ttt.g4
+++ core/src/main/antlr4/com/pojosontheweb/ttt/Ttt.g4
@@ -1,5 +1,21 @@
 // Define a grammar called Hello
 grammar Ttt;
-r  : 'hello' ID ;         // match keyword hello followed by an identifier
-ID : [a-z]+ ;             // match lower-case identifiers
-WS : [ \t\r\n]+ -> skip ; // skip spaces, tabs, newlines
+
+r : args ;
+
+arg : identifier ':' className ;
+
+className : identifier ( '.' identifier )* ;
+
+args : '<%(' arg (',' arg)* ')%>' ;
+
+identifier : LETTER (LETTER | DIGIT)* ;
+
+LETTER : 'A'..'Z' | 'a'..'z' ;
+DIGIT : '0'..'9' ;
+WS : [ \t\r\n]+ -> skip;
+
+
+//r  : 'hello' ID ;         // match keyword hello followed by an identifier
+//ID : [a-z]+ ;             // match lower-case identifiers
+//WS : [ \t\r\n]+ -> skip ; // skip spaces, tabs, newlines
--- core/src/test/java/com/pojosontheweb/ttt/AntlrTest.java
+++ core/src/test/java/com/pojosontheweb/ttt/AntlrTest.java
@@ -1,24 +1,47 @@
 package com.pojosontheweb.ttt;
 
+import org.antlr.v4.gui.TreeViewer;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.junit.Test;
 
+import javax.swing.*;
 import java.io.StringReader;
+import java.util.Arrays;
 
 public class AntlrTest {
 
     @Test
     public void doTest() throws Exception{
 
-        String s = "hello there";
+        String s = "<%( foo1:com.xyz.Bar, x: int )%>";
         ANTLRInputStream input = new ANTLRInputStream(new StringReader(s)); // create a lexer that feeds off of input CharStream
         TttLexer lexer = new TttLexer(input); // create a buffer of tokens pulled from the lexer
         CommonTokenStream tokens = new CommonTokenStream(lexer); // create a parser that feeds off the tokens buffer
         TttParser parser = new TttParser(tokens);
         ParseTree tree = parser.r(); // begin parsing at init rule
         System.out.println(tree.toStringTree(parser)); // print LISP-style tree
-
     }
+
+//    public static void main(String[] args) throws Exception {
+//        String s = "<%( foo : com.xyz.Foo )%>";
+//        ANTLRInputStream input = new ANTLRInputStream(new StringReader(s)); // create a lexer that feeds off of input CharStream
+//        TttLexer lexer = new TttLexer(input); // create a buffer of tokens pulled from the lexer
+//        CommonTokenStream tokens = new CommonTokenStream(lexer); // create a parser that feeds off the tokens buffer
+//        TttParser parser = new TttParser(tokens);
+//        ParseTree tree = parser.r(); // begin parsing at init rule
+//
+//        //show AST in GUI
+//        JFrame frame = new JFrame("Antlr AST");
+//        JPanel panel = new JPanel();
+//        TreeViewer viewr = new TreeViewer(Arrays.asList(
+//            parser.getRuleNames()),tree);
+//        viewr.setScale(1.5);//scale a little
+//        panel.add(viewr);
+//        frame.add(panel);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(200,200);
+//        frame.setVisible(true);
+//    }
 }
